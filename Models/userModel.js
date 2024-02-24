54be13cbc117f4869839e34d4466623e05de9a69
Udhay <udhaypareek@gmail.com>
2024-02-24 15:32:57 +0530

    Added config and controller files.

--- Models/userModel.js
+++ Models/userModel.js
@@ -1,8 +1,7 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
-import { Schema } from "mongoose";
 
-const userSchema = Schema(
+const userSchema = mongoose.Schema(
   {
     name: { type: "String", required: true },
     email: { type: "String", unique: true, required: true },
@@ -21,18 +20,18 @@ const userSchema = Schema(
   { timestaps: true }
 );
 
-// userSchema.methods.matchPassword = async function (enteredPassword) {
-//   return await bcrypt.compare(enteredPassword, this.password);
-// };
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 
-// userSchema.pre("save", async function (next) {
-//   if (!this.isModified) {
-//     next();
-//   }
+userSchema.pre("save", async function (next) {
+  if (!this.isModified) {
+    next();
+  }
 
-//   const salt = await bcrypt.genSalt(10);
-//   this.password = await bcrypt.hash(this.password, salt);
-// });
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
 
 const User = mongoose.model("User", userSchema);
 
--- index.js
+++ index.js
@@ -1,11 +1,43 @@
-const connectToMongo = require('./db');
+const connectToMongo = require('./config/db');
 const express = require('express');
 const cors = require('cors')
+const userRoutes = require('./routes/userRoutes')
 
 connectToMongo();
 const app = express();
 // Cors error resolution.
 app.use(cors());
+
+// Routes ::
+
+app.use("/api/user", userRoutes);
+// app.use("/api/chat", chatRoutes);
+// app.use("/api/message", messageRoutes);
+
+
+
+// // --------------------------deployment------------------------------
+
+// const __dirname1 = path.resolve();
+
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname1, "/frontend/build")));
+
+//   app.get("*", (req, res) =>
+//     res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+//   );
+// } else {
+//   app.get("/", (req, res) => {
+//     res.send("API is running..");
+//   });
+// }
+
+// --------------------------deployment------------------------------
+
+// Error Handling middlewares
+// app.use(notFound);
+// app.use(errorHandler);
+
 const port = 5000;
 
 app.use(express.json());
