dcebddf8b9d17d524fcfbc1ae7def1f4dd6f83d6
Udhay <udhaypareek@gmail.com>
2024-02-25 12:30:44 +0530

    Added message controller.

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,5 +1,5 @@
 const asyncHandler = require("express-async-handler");
-const User = require("../Models/userModel");
+const User = require("../models/userModel");
 const generateToken = require("../config/generateToken");
 
 //@description     Get or Search all users
